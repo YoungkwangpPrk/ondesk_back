f864fcf46704234324b26274981816d2896c0607
pyk2191 <pyk2191@nate.com>
2025-04-10 12:30:15 +0900

    first commit

---
+++ src/main/java/com/ondesk/ondesk/OndeskApplication.java
@@ -0,0 +1,13 @@
+package com.ondesk.ondesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ondesk/ondesk/OndeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ondesk.ondesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
